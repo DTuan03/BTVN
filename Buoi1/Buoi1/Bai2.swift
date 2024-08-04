1a02de80ef295fcbe8466e7ab7efbf89a6b5e1c6
Đặng Anh Tuấn <137882845+DTuan03@users.noreply.github.com>
2024-08-04 14:30:48 +0700

    btvn

---
+++ Buoi1/Buoi1/Bai2.swift
@@ -0,0 +1,24 @@
+//
+//  Bai2.swift
+//  
+//
+//  Created by Tuấn on 1/8/24.
+//
+
+import Foundation
+
+func bai2() {
+    print("Nhap so m: ", terminator: "")
+    
+    let m = Int(readLine() ?? "") ?? 0
+    
+    var giaiThua: Int = 1
+    var n:Int = 0
+    
+    while(giaiThua < m) {
+        n += 1
+        giaiThua *= n
+    }
+    
+    print("So can tim la: \(n)")
+}
---
+++ Buoi1/Buoi1/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Buoi1
+//
+//  Created by Tuấn on 4/8/24.
+//
+
+import Foundation
+
+bai1()
+bai2()
